6d7fe7b7cb77fd6f1786b4e35da4b3c6cdd8292a
hongjimin00 <chunsim015@gmail.com>
2024-04-04 17:02:30 +0900

    feat: separate api key code file

---
+++ js/apikey.js
@@ -0,0 +1,8 @@
+// api key
+import config from "../key.js";
+
+const NAVERMAP_API_KEY = config.navermapapi;
+const mapScript = document.createElement("script");
+mapScript.type = "text/javascript";
+mapScript.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${NAVERMAP_API_KEY}`;
+document.head.appendChild(mapScript);

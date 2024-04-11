c60d9a763add48a23762319f551400b0c0cb458c
hongjimin00 <chunsim015@gmail.com>
2024-04-11 17:13:32 +0900

    feat: add show list apikey

--- js/apikey.js
+++ js/apikey.js
@@ -6,3 +6,7 @@ const mapScript = document.createElement("script");
 mapScript.type = "text/javascript";
 mapScript.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${NAVERMAP_API_KEY}`;
 document.head.appendChild(mapScript);
+
+const KCISA_API_KEY = config.kcisaapi;
+
+export default KCISA_API_KEY;
